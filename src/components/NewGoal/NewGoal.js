32060015dd3df741a96f3d8e0efb045b55129383
Abdou <abdelmoula.chakhkhar@uit.ac.ma>
2021-02-02 20:13:13 +0100

    first commit

---
+++ src/components/NewGoal/NewGoal.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import './NewGoal.css';
+
+const NewGoal = ({onAddGoal}) => {
+  const [enteredText, setEnteredText] = useState('');
+
+  const addGoalHandler = e => {
+    e.preventDefault();
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: enteredText
+    };
+
+    setEnteredText('');
+
+   onAddGoal(newGoal);
+  };
+
+  const textChangeHandler = e => {
+    setEnteredText(e.target.value);
+  };
+
+  return (
+    <form className="new-goal" onSubmit={addGoalHandler}>
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
+      <button type="submit">Add Goal</button>
+    </form>
+  );
+};
+
+export default NewGoal;
